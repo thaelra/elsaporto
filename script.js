da337b614be603f5623dc6dc951a797167045388
thaelra <zachariaselsa@gmail.com>
2024-10-02 21:07:22 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+    // Pagination bullets
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+});
